eb16f9ab83d13a0bad01a1aea96004ad6627547c
Burak <79156797+tscburak@users.noreply.github.com>
2021-04-27 10:34:10 +0300

    Add files via upload

---
+++ practice1/src/practice1/Course.java
@@ -0,0 +1,26 @@
+package practice1;
+
+public class Course {
+
+	public Course()
+	{
+		System.out.println("Course class has been executed");
+	}
+	
+	public Course(String courseName,String courseTeacher, int courseProgress, int courseId)
+	{
+		this.courseId=courseId;
+		this.courseName=courseName;
+		this.courseTeacher=courseTeacher;
+		this.courseProgress=courseProgress;
+		
+	}
+	
+	String courseName;
+	String courseTeacher;
+	int courseProgress;
+	int courseId;
+	
+	
+	
+}
---
+++ practice1/src/practice1/CourseManager.java
@@ -0,0 +1,9 @@
+package practice1;
+
+public class CourseManager {
+
+	public void addCourse(Course course){
+		System.out.println("The course (" + course.courseName + ") has been added");
+	}
+	
+}
