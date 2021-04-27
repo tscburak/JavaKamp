50a4b2dd0318d2bae5e90064c177f1622811ac93
Burak <79156797+tscburak@users.noreply.github.com>
2021-04-27 10:55:25 +0300

    Delete practice1 directory

--- practice1/src/practice1/CourseManager.java
+++
@@ -1,9 +0,0 @@
-package practice1;
-
-public class CourseManager {
-
-	public void addCourse(Course course){
-		System.out.println("The course (" + course.courseName + ") has been added");
-	}
-	
-}
